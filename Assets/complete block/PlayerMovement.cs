6da1b9793580d99708be570f382bc9c4dfa56023
Даниил Ликутин <likutindan@gmail.com>
2024-07-20 22:37:45 +0300

    Нормальное передвижение

--- Assets/complete block/PlayerMovement.cs
+++ Assets/complete block/PlayerMovement.cs
@@ -11,12 +11,23 @@ public class NewBehaviourScript : MonoBehaviour
     public float speed = 1.0f;
     public float thrust = 1.0f;
 
+    private float HorizontalMove;
     private Rigidbody2D playerbody;
-    private Vector2 moveVector;
     private bool OnGround = false;
-    private void Awake()
+    private void Start()
     {
         playerbody = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && OnGround)
+        {
+            Debug.Log("Ïðûæîê");
+            playerbody.AddForce(playerbody.transform.up*thrust, ForceMode2D.Impulse);
+        }
+
+        HorizontalMove = Input.GetAxisRaw("Horizontal");
+
     }
     void FixedUpdate()
     {
@@ -26,16 +37,11 @@ public class NewBehaviourScript : MonoBehaviour
             OnGround = true;
         }
         else OnGround = false;
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        if ((horizontal > 0.1f || horizontal < -0.1f) && Mathf.Abs(playerbody.velocity.x) < 1.5f)
-        {
-            playerbody.AddForce(new Vector2(horizontal * speed, 0f), ForceMode2D.Impulse);
-        }
-        //Mathf.Abs(playerbody.velocity.y) < 0.005f
-        if (vertical > 0.1f && OnGround) 
-        {
-            playerbody.AddForce(new Vector2(0f, vertical * thrust),ForceMode2D.Impulse);
-        }
+        
+        Vector2 targetVelocity = new Vector2(HorizontalMove * speed,playerbody.velocity.y);
+        playerbody.velocity = targetVelocity;
+
+        
+
     }
 }
